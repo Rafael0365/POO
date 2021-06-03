1bf412de46c0315d3b626fb0437c1fcd351665df
Rafael0365 <rafaelantonio0365@gmail.com>
2021-06-02 22:51:26 -0300

    classe professores adicionado

---
+++ ATV5_Rafael Antonio de Lima Silva/src/cursos/Professor.java
@@ -0,0 +1,62 @@
+package cursos;
+
+import java.util.Arrays;
+
+public class Professor extends Pessoa{
+	private int numCursos;
+	private String[] cursos;
+	
+	public Professor(String nome, String endereco, int tamanho) {
+		super(nome, endereco);
+		this.cursos = new String[tamanho];
+	}
+	
+	public void addCurso(String c) {
+		this.cursos[this.numCursos] = c;
+		this.numCursos = this.numCursos + 1;
+	}
+	
+	public String procuraCurso(String num) {
+		int i = this.procurarIndice(num);
+		String resultado = null;
+		if(i != this.numCursos) {
+			resultado = this.cursos[i];
+		}
+		return resultado;
+	}
+
+	public void removeCurso(String num) {
+		int i = this.procurarIndice(num);
+		if(i != this.numCursos) {
+			this.cursos[i] = this.cursos[this.numCursos - 1];
+			this.cursos[this.numCursos - 1] = null;
+			this.numCursos = this.numCursos - 1;
+			System.out.println("String" + num + "removida");
+		}else {
+			System.out.println("String não existe.");
+		}
+	}
+	
+	private int procurarIndice(String num) {
+		int i = 0;
+		boolean achou = false;
+		while((!achou) && (i < this.numCursos)) {
+			if(num.equals(this.cursos[i])) {
+				achou = true;
+			}else {
+				i = i + 1;
+			}
+		}
+				
+		return i;
+	}
+
+	@Override
+	public String toString() {
+		return "Repositoriocursos [cursos=" + Arrays.toString(cursos) + ", numCursos=" + numCursos + "]";
+	}
+		
+		
+		
+}
+
