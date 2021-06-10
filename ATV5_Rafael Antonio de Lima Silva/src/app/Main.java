7905bb2977f95a60c111b6a173397f4c102c2daf
Rafael0365 <rafaelantonio0365@gmail.com>
2021-06-10 03:40:57 -0300

    metodo estatico e exibir polimorficamente adicionado

--- ATV5_Rafael Antonio de Lima Silva/src/app/Main.java
+++ ATV5_Rafael Antonio de Lima Silva/src/app/Main.java
@@ -1,6 +1,7 @@
 package app;
 
 import java.util.ArrayList;
+
 import cursos.*;
 
 public class Main {
@@ -32,6 +33,53 @@ public class Main {
 		pessoas.add(professor1);
 		pessoas.add(professor2);
 		
+		for(int i = 0; i<pessoas.size(); i++) {
+			String str = pessoas.get(i).toString();
+			System.out.println(str);
+		}
+		
+		String test = varrerPessoas(pessoas);
+		System.out.println(test);
 	}
+	
+	private static String varrerPessoas(ArrayList<Pessoa> pessoas) {
+			
+			Professor professor = new Professor(null, null);
+			Estudante estudante = new Estudante(null, null);
+			String string = "";
+			
+				
+				for(int i = 0; i< pessoas.size(); i++) {
+						
+					 if(professor.getClass() == pessoas.get(i).getClass()) {
+						 Professor teacher = (Professor) pessoas.get(i);
+						 
+						 for(int y = 0; y < teacher.getCursos().size(); y++) {
+							string += "Curso: "+ teacher.getCursos().get(y)+"\n";
+							string += "Professor: " + teacher.getNome()+"\n";
+							string += "Alunos: \n";
+									
+								for(int z = 0; z < pessoas.size(); z++) {
+									
+									if(estudante.getClass() == pessoas.get(z).getClass()) {
+										Estudante student = (Estudante) pessoas.get(z);
+										
+										for(int x = 0; x < student.getCursos().size(); x++) {
+											
+											if(student.getCursos().get(x).equals(teacher.getCursos().get(y))) {
+												string += student.getNome()+"\n";
+											}
+										}
+									}
+								}
+								string +="------------------------- \n";
+						 }
+					 }
+				}
+				
+				return string;
+		}
 
 }
+
+
